9b4a0a36ea293efb7ba88546fd53e5dd7f6a84c2
Felipe Castillo <felipecastillo.snk@gmail.com>
2024-12-11 22:36:59 -0300

    createUser for admin

--- src/pages/Usuarios.js
+++ src/pages/Usuarios.js
@@ -1,34 +1,84 @@
+// Implementado lo de crear usuarios, falta ocultar vista para los que no son administradores y
+// arreglar un pequeño bug que al momento de crear usuario no se refleja en el front al instante
 import React, { useState, useEffect } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography, Modal, TextField, Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { useAuth } from '../context/AuthContext';
+import axios from 'axios';
 
 const Usuarios = () => {
-    const [usuarios, setUsuarios] = useState([]);
+    const { auth } = useAuth();
+    const [users, setUsers] = useState([]);
+    const [roles, setRoles] = useState([]);
+    const [statuses, setStatuses] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [openModal, setOpenModal] = useState(false);
+    const [newUser, setNewUser] = useState({ username: '', email: '', password: '', full_name: '', company_id: '', roleId: '', statusId: '' });
 
     useEffect(() => {
-        const obtenerUsuarios = async () => {
+        const fetchOptions = async () => {
             try {
-                // simulacion de datos mientras el backend no esta conectado
-                const dataSimulada = [
-                    { id: 1, username: 'usuario1', email: 'usuario1@mail.com', role: 'admin' },
-                    { id: 2, username: 'usuario2', email: 'usuario2@mail.com', role: 'user' },
-                ];
-                setUsuarios(dataSimulada);
+                const [rolesRes, statusesRes] = await Promise.all([
+                axios.get('http://localhost:4000/role', { headers: { Authorization: `Bearer ${auth.token}` } }),
+                axios.get('http://localhost:4000/status', { headers: { Authorization: `Bearer ${auth.token}` } }),
+                ]);
+                setRoles(rolesRes.data);
+                setStatuses(statusesRes.data);
+            } catch (err) {
+                console.error("Error al obtener opciones:", err);
+                setError('Error al cargar las opciones');
+            }
+            };
+            fetchOptions();
+        }, [auth.token]);
 
-                //descomentar esto cuando el backend este listo
-                /*
-                const response = await fetch('/api/usuarios');
-                if (!response.ok) {
-                  throw new Error('Error al obtener usuarios');
-                }
-                const data = await response.json();
-                setUsuarios(data);
-                */
-            } catch (error) {
-                console.error('Error al obtener usuarios:', error);
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get('http://localhost:4000/user/', {
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`,
+                    },
+                });
+                setUsers(response.data);
+            } catch (err) {
+                console.error("Error al obtener usuarios:", err);
+                setError('Error al obtener usuarios');
+            } finally {
+                setLoading(false);
             }
         };
-        obtenerUsuarios();
-    }, []);
+        fetchUsers();
+    }, [auth.token]);
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+            setNewUser({ ...newUser, [name]: value });
+    };
+
+    const handleCreateUser = async () => {
+        try {
+            const response = await axios.post('http://localhost:4000/company/createUser', newUser, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`,
+                },
+            });
+            setUsers([...users, response.data]);
+            setNewUser({ username: '', email: '', password: '', full_name: '', company_id: 1, roleId: '', statusId: '' });
+            handleCloseModal();
+        } catch (err) {
+            console.error("Error al crear usuario:", err);
+            setError('Error al crear usuario');
+        }
+    };
 
     const handleDelete = (id) => {
         console.log(`Usuario con id ${id} eliminado`);
@@ -39,27 +89,32 @@ const Usuarios = () => {
             <Typography variant="h2" gutterBottom>
                 Gestión de Usuarios
             </Typography>
+            <Button variant="contained" color="primary" style={{ marginBottom: '20px' }} onClick={handleOpenModal}>
+                Agregar Usuario
+            </Button>
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Nombre</TableCell>
+                            <TableCell>Nombre Completo</TableCell>
                             <TableCell>Email</TableCell>
                             <TableCell>Rol</TableCell>
+                            <TableCell>Estado</TableCell>
                             <TableCell>Acciones</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {usuarios.map((usuario) => (
-                            <TableRow key={usuario.id}>
-                                <TableCell>{usuario.username}</TableCell>
-                                <TableCell>{usuario.email}</TableCell>
-                                <TableCell>{usuario.role}</TableCell>
+                        {users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>{user.full_name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{user.role?.name || 'Sin rol'}</TableCell>
+                                <TableCell>{user.status?.name || 'Sin estado'}</TableCell>
                                 <TableCell>
                                     <Button variant="contained" color="primary" sx={{ marginRight: 1 }}>
                                         Editar
                                     </Button>
-                                    <Button variant="contained" color="secondary" onClick={() => handleDelete(usuario.id)}>
+                                    <Button variant="contained" color="secondary" onClick={() => handleDelete(user.id)}>
                                         Eliminar
                                     </Button>
                                 </TableCell>
@@ -67,6 +122,87 @@ const Usuarios = () => {
                         ))}
                     </TableBody>
                 </Table>
+
+                <Modal open={openModal} onClose={handleCloseModal}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                >
+                    <Typography variant="h6" gutterBottom>
+                        Agregar Usuario
+                    </Typography>
+                    <TextField
+                        placeholder="Usuario"
+                        name="username"
+                        value={newUser.username}
+                        onChange={handleInputChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        placeholder="Correo Electronico"
+                        name="email"
+                        value={newUser.email}
+                        onChange={handleInputChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        placeholder="Contraseña"
+                        name="password"
+                        value={newUser.password}
+                        onChange={handleInputChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <TextField
+                        placeholder="Nombre Completo"
+                        name="full_name"
+                        value={newUser.full_name}
+                        onChange={handleInputChange}
+                        fullWidth
+                        margin="normal"
+                    />
+                    <FormControl fullWidth margin="normal">
+                        <InputLabel>Rol</InputLabel>
+                        <Select
+                        label="Rol"
+                        name="roleId"
+                        value={newUser.roleId}
+                        onChange={handleInputChange}
+                        >
+                        {roles.map((role) => (
+                            <MenuItem key={role.id} value={role.id}>{role.name}</MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth margin="normal">
+                        <InputLabel>Estado</InputLabel>
+                        <Select
+                        label="Estado"
+                        name="statusId"
+                        value={newUser.statusId}
+                        onChange={handleInputChange}
+                        >
+                        {statuses.map((status) => (
+                            <MenuItem key={status.id} value={status.id}>{status.name}</MenuItem>
+                        ))}
+                        </Select>
+                    </FormControl>
+                    <Button variant="contained" color="primary" onClick={handleCreateUser}>
+                        Agregar
+                    </Button>
+                </Box>
+            </Modal>
+
             </TableContainer>
         </div>
     );
